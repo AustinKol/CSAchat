7819b6103aa22ea88a45761bb8b70572afaede3c
austinwang2004@hotmail.com <austinwang2004@hotmail.com>
2021-01-31 12:53:57 -0500

    advanced Home.tsx/created new folder and files

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import '../App.css';
+import {makeStyles} from '@material-ui/core/styles';
+import Header from './HomeComponents/Header';
+import Chatbox from './HomeComponents/Chatbox';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        minHeight: '100vh',
+        backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg.png'})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+    },
+}));
 function Home() {
-    return (<p>Home</p>);
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <Header />
+            <Chatbox />
+
+        </div>
+    );
 }
 
 export default Home;
