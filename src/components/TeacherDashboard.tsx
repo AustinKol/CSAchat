e0e5a07e1dd6a7031990e2fd3e5a3f4f40520455
AustinKol <austinwang2004@hotmail.com>
2021-03-11 20:27:10 -0500

    teacher

--- src/components/TeacherDashboard.tsx
+++ src/components/TeacherDashboard.tsx
@@ -1,5 +1,59 @@
+import firebase from "firebase";
+import React from "react";
+import { ListGroup } from "react-bootstrap";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
 function TeacherDashboard() {
-    return (<></>);
+    const firestore = firebase.firestore();
+
+    const users = firestore.collection('users');
+    const queue = users.orderBy("timeCreated", "desc");
+
+
+
+    const [userOrder] = useCollectionData(queue);
+
+    console.log(userOrder);
+    
+
+
+
+
+
+    return (
+    
+        <>
+
+            {userOrder?.map((user) => {
+
+                const uidCreated = firestore.collection('users').get
+                    
+                return(
+                    <ListGroup horizontal>
+                        <ListGroup.Item>This</ListGroup.Item>
+                        <ListGroup.Item></ListGroup.Item>
+                        <ListGroup.Item>uidCreated</ListGroup.Item>
+                        <a href='selfhelp'>
+                            <ListGroup.Item>horizontally!</ListGroup.Item>
+                        </a>
+                        
+                    </ListGroup>
+
+                )
+
+                    
+                })
+            }
+                    
+
+
+    
+    
+    
+        </>
+    );
 }
 
 export default TeacherDashboard;
+
+
