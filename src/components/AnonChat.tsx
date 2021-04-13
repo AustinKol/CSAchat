4b28e155283ee06a5988e68eb39bd65a1004aeea
Green-Robot-Dev-Studios <nficara01@gmail.com>
2021-04-13 16:28:26 -0400

    Added proper room ending functionality

--- src/components/AnonChat.tsx
+++ src/components/AnonChat.tsx
@@ -1,13 +1,25 @@
 import firebase from "firebase";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { Redirect, useHistory } from "react-router-dom";
 import ChatPage from "./ChatPage";
 
 function AnonChat() {
     const auth = firebase.auth();
     const [user, loading] = useAuthState(auth);
-
     const hasRun = useRef(false);
+    //const [signingOut, setSigningOut] = useState(false);
+    let hist = useHistory();
+
+    async function signOut() {
+        //setSigningOut(true);
+        console.log("[II] Signing out")
+        setTimeout(function() {
+            auth.signOut();
+            hist.push("/");
+        }, 3000);
+    }
+
     useEffect(() => {
         if (!hasRun.current) {
             auth.signInAnonymously();
@@ -17,7 +29,7 @@ function AnonChat() {
     }, [auth]);
     
     return (<>
-        {!loading && user?.isAnonymous ? <ChatPage uid={user.uid} person={0}/> : "Telechargement en cours..."}
+        {!loading && user?.isAnonymous ? <ChatPage signout={signOut} uid={user.uid} person={0}/> : "Telechargement en cours..."}
     </>);
 }
 
--- src/components/ChatPage.tsx
+++ src/components/ChatPage.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -10,12 +10,14 @@ import { Button, Container, Form, Row, Col, ListGroup } from 'react-bootstrap';
 
 function ChatPage(props:any) {
     const firestore = firebase.firestore();
-
+    
     const [formValue, setFormValue] = useState('');
     const scrollThing = useRef<HTMLDivElement>(null!);
 
     const userMessages = firestore.collection('users').doc(props.uid).collection("messages");
     const query = userMessages.orderBy('time');
+
+    const roomClose = "Ce chat a été fermé par le modérateur.";
 
     const sendMessage = async (msg:any, person:any) => {
         firestore.collection('users').doc(props.uid).get().then((doc) => {
@@ -33,22 +35,49 @@ function ChatPage(props:any) {
         });
         setFormValue('');
         // Scroll control
-        scrollThing.current.scrollIntoView({behavior: 'smooth'});
     }
 
     async function killRoom() {
         //firestore.collection('users').doc(props.uid).delete();
-        await sendMessage("Ce chat a été fermé par le modérateur.", 1);
+        await sendMessage(roomClose, 1);
         await firestore.collection('users').doc(props.uid).update({isFinished: true});
         props.thread("");
     }
 
     const [messages] = useCollectionData(query);
-    console.log(messages);
+    //console.log(messages);
+
+
+    const left = {maxWidth: "50%", wordWrap: "break-word", placeSelf: "flex-start", borderColor: "#007bff", borderRadius: "3px", marginTop: "3px", borderWidth: "1px"};
+    const right = {maxWidth: "50%", wordWrap: "break-word", placeSelf: "flex-end", borderRadius: "3px", marginTop: "3px", borderWidth: "1px"};
+    const center = {wordWrap: "break-word", placeSelf: "flex-center", borderColor: "#F01a0a", borderRadius: "3px", marginTop: "3px", borderWidth: "1px"};
+
+    const listMessages = messages && messages.map((msg:any, i) => {
+        var position:any = left;
+        if (msg["person"]^props.person) {
+            position = left;
+        } else {
+            position = right;
+        }
+        if (msg["text"] === "") return(<></>);
+        if (msg["text"] === roomClose && props.person === 0) {
+            props.signout();
+            position = center;
+        }
+        return(
+            <ListGroup.Item style={position} key={i}>
+                {msg["text"]}
+            </ListGroup.Item>
+        );
+    });
+
+    useEffect(() => {
+        scrollThing.current && scrollThing.current.scrollIntoView({behavior: 'smooth'});
+    });
 
     return (
         <>
-            <Container>
+            <Container className="fixed-top">
                 {
                     props.person === 1 ?
                         <Row>
@@ -61,21 +90,18 @@ function ChatPage(props:any) {
                         </Row>
                     : <></>
                 }
-
+            </Container>
+            <Container>
                 <Row>
                     <Col>
-                        <ListGroup>
-                            {messages && messages.map((msg:any, i) => {
-                                return(<ListGroup.Item style={msg["person"]^props.person ? {placeSelf: "flex-start", borderColor: "#007bff", borderRadius: "3px", marginTop: "3px", borderWidth: "1px"} : {placeSelf: "flex-end", borderRadius: "3px", marginTop: "3px", borderWidth: "1px"}} key={i}>
-                                    {msg["text"]}
-                                </ListGroup.Item>);
-                            })}
+                        <ListGroup style={{paddingBottom: "100px"}}>
+                            {listMessages}
                             <div ref={scrollThing}></div>
                         </ListGroup>
                     </Col>
                 </Row>
             </Container>
-            <Container className="fixed-bottom">
+            <Container className="fixed-bottom" style={{backgroundColor: "white"}}>
                     <Form className="input-group mb-3" onSubmit={async (e) => {e.preventDefault(); sendMessage(formValue, props.person)}}>
                         <Form.Control value={formValue} onChange={c => setFormValue(c.target.value)} type="text" placeholder="Message" />
                         <div className="input-group-append">
